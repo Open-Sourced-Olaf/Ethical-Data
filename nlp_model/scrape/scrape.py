be19489498c6b07304e559cc30529939dcf75f6f
gladishd <gladish.dean@gmail.com>
2021-04-02 03:26:45 -0400

    added naive bayes sentiment analysis model!

--- nlp_model/scrape/scrape.py
+++ nlp_model/scrape/scrape.py
@@ -27,6 +27,7 @@ def scrape_google(url):
     result = {'text': text}
     return result
 
+
 def scrape_twitter(url):
 
     html = urllib.request.urlopen(url).read()
@@ -49,6 +50,7 @@ def scrape_twitter(url):
     result = {'text': text}
     return result
 
+
 def scrape_amazon(url):
 
     html = urllib.request.urlopen(url).read()
@@ -68,13 +70,14 @@ def scrape_amazon(url):
         clean_text.append(sentence.text)
 
     # Combine all elements in clean_text array into a single string
-    
+
     clean_text = filter(None, clean_text)
     text = ' '.join(clean_text)
 
     # Put results into a dictionary
     result = {'text': text}
     return result
+
 
 def scrape_microsoft(url):
 
@@ -102,29 +105,28 @@ def scrape_microsoft(url):
     result = {'text': text}
     return result
 
+
 if __name__ == '__main__':
     scrapedText = scrape_google("https://policies.google.com/privacy")
-    with open('google.csv', 'w+', newline='') as file:
+    with open('google.csv', 'w+', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([scrapedText['text']])
 
     scrapedText = scrape_twitter('https://twitter.com/en/privacy')
     print(scrapedText)
-    with open('twitter.csv', 'w+', newline='') as file:
+    with open('twitter.csv', 'w+', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([scrapedText['text']])
 
     # Amazon detected I'm scraping so I got a 503
     # scrapedText = scrape_amazon('https://www.amazon.com/gp/help/customer/display.html?nodeId=GX7NJQ4ZB8MHFRNJ')
-    # with open('amazon.csv', 'w+', newline='') as file:
+    # with open('amazon.csv', 'w+', newline='', encoding='utf-8') as file:
     #     writer = csv.writer(file)
     #     writer.writerow([scrapedText['text']])
 
-    scrapedText = scrape_microsoft('https://privacy.microsoft.com/en-ca/privacystatement')
+    scrapedText = scrape_microsoft(
+        'https://privacy.microsoft.com/en-ca/privacystatement')
     print(scrapedText)
-    with open('microsoft.csv', 'w+', newline='') as file:
+    with open('microsoft.csv', 'w+', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([scrapedText['text']])
-
-
-
